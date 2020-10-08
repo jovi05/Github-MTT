339bf79d391eed3a06678e8464df6d9749d5f5f8
jovi05 <72500796+jovi05@users.noreply.github.com>
2020-10-08 18:31:59 +0700

    Create listdonasi.php

---
+++ listdonasi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>List Sumbangan Buku</h2>
+
+<p>Pilih kota :</p>
+
+<form action="/action_page.php">
+  <input list="kota" name="kota">
+  <datalist id="kota">
+    <option value="Aceh">
+    <option value="Medan">
+    <option value="Surabaya">
+    <option value="Jakarta">
+    <option value="Bandung">
+  </datalist>
+  <input type="submit">
+</form>
+
+
+</body>
+</html>
